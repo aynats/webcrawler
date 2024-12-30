6178288fc0fcffe3f2146568939054b26ae5a3c4
Tatiana <tanya-sok-05@mail.ru>
2024-12-30 20:18:19 +0500

    хелп в кли

--- CLI.py
+++ CLI.py
@@ -11,13 +11,13 @@ logging.basicConfig(
 
 
 @click.command()
-@click.option('--scan', '-s', required=True, type=str, help='URL сканируемого сайта или файл .txt с несколькими URL-ами'
+@click.option('--scan', '-s', required=True, type=str, help='URL сканируемого сайта или файл .txt с несколькими URL-ами\n'
                                                             'Пример: --scan https://example.com или --scan urls.txt')
 @click.option('--depth', '-d', default=3, type=int, help='Глубина сканирования ресурса', show_default=True)
-@click.option('--path', '-p', default='', help='Директория для скачивания файлов'
+@click.option('--path', '-p', default='', help='Директория для скачивания файлов\n'
                                                'Пример: --path C:/Users/User/directory')
 @click.option('--bots', '-b', default=4, help="Количество ботов для обхода", show_default=True)
-@click.option('--domain', '-dm', default='', type=str, help="Переход только по ссылкам указанного домена"
+@click.option('--domain', '-dm', default='', type=str, help="Переход только по ссылкам указанного домена\n"
                                                             'Пример: --domain example.org или --domain domains.txt')
 async def crawl(scan: str, depth: int, path: str, bots: int, domain: str):
     async def run_crawler():
